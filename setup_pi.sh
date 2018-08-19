7d4fc08245ef634b701b5ce41f7f9609aaee34b9
Marco Tijbout <32824067+MTijbout@users.noreply.github.com>
2018-08-19 23:16:03 +0200

    Create setup_pi.sh

---
+++ setup_pi.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+##
+## Welcome to the Pi setup script
+##
+
+##
+## Version 1.0
+##
+
+#
+# Changelog
+#
+# 1.0 - Optimized for Raspberry Pi usage
+#
+
+## Download the latest version of the scripts
+
+curl https://raw.githubusercontent.com/MTijbout/usefull-files/master/bash_profile > .bash_profile
+curl https://raw.githubusercontent.com/MTijbout/usefull-files/master/bash_aliases > .bash_aliases
