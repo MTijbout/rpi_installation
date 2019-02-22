403c222194a1d4eeff9dbd34dba5937c239f9cdb
Marco Tijbout <32824067+MTijbout@users.noreply.github.com>
2019-02-22 15:34:10 +0100

    Update pi_setup_raspbian.sh

--- pi_setup_raspbian.sh
+++ pi_setup_raspbian.sh
@@ -5,12 +5,13 @@
 ##
 
 ##
-## Version 1.2
+## Version 1.3
 ##
 
 #
 # Changelog
 #
+# 1.3 - Added the hostRename.sh script and make it executable.
 # 1.2 - Added the modified bashrc-rpi implementation.
 # 1.1 - Fixed curl error and made it silent.
 # 1.0 - Optimized for Raspberry Pi usage.
@@ -21,6 +22,8 @@
 wget -O .bash_aliases https://raw.githubusercontent.com/MTijbout/usefull-files/master/bash_aliases_rpi
 wget -O .bash_profile https://raw.githubusercontent.com/MTijbout/usefull-files/master/bash_profile
 wget -O .bashrc https://raw.githubusercontent.com/MTijbout/usefull-files/master/bashrc-rpi
+wget -O hostRename.sh https://raw.githubusercontent.com/MTijbout/usefull-files/master/hostRename.sh
+chmod +x hostRename.sh
 
 . ~/.bash_profile
 
